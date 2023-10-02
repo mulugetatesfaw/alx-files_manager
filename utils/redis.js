4a7f5b8acba655bde1046908956b4df6526e845a
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-10-02 00:17:47 -0700

    create a file redis.js that contains the class RedisClient

---
+++ utils/redis.js
@@ -0,0 +1,63 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.error('Redis Error:', error);
+    });
+  }
+
+  async isAlive() {
+    return new Promise((resolve) => {
+      this.client.ping('alive', (error, result) => {
+        if (error) {
+          resolve(false);
+        } else {
+          resolve(result === 'alive');
+        }
+      });
+    });
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
