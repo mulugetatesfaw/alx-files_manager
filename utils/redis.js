95b9ac658c329e80f12c9fa6fe27314e63dcb275
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-10-04 13:22:49 -0400

    redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,63 +1,88 @@
 const redis = require('redis');
 
 class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
   constructor() {
     this.client = redis.createClient();
+    this.isClientConnected = true;
 
-    this.client.on('error', (error) => {
-      console.error('Redis Error:', error);
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.setConnectedStatus(false);
+    });
+
+    this.client.on('connect', () => {
+      this.setConnectedStatus(true);
     });
   }
 
-  async isAlive() {
-    return new Promise((resolve) => {
-      this.client.ping('alive', (error, result) => {
-        if (error) {
-          resolve(false);
-        } else {
-          resolve(result === 'alive');
-        }
-      });
-    });
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.isClientConnected;
   }
 
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {Promise<String | Object>}
+   */
   async get(key) {
     return new Promise((resolve, reject) => {
-      this.client.get(key, (error, result) => {
-        if (error) {
-          reject(error);
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
         } else {
-          resolve(result);
+          resolve(value);
         }
       });
     });
   }
 
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (error, result) => {
-        if (error) {
-          reject(error);
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
         } else {
-          resolve(result);
+          resolve();
         }
       });
     });
   }
 
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
   async del(key) {
     return new Promise((resolve, reject) => {
-      this.client.del(key, (error, result) => {
-        if (error) {
-          reject(error);
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
         } else {
-          resolve(result);
+          resolve();
         }
       });
     });
+  }
+
+  setConnectedStatus(status) {
+    this.isClientConnected = status;
   }
 }
 
 const redisClient = new RedisClient();
-
 module.exports = redisClient;
